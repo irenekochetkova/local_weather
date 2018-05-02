c7d82a64653b3e96bf5238be5ccfb4a313332fd8
Irina <irenekochetkova@gmail.com>
2018-05-01 23:06:33 -0700

    added conversion of Celsius to Fahrenheit

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
+var mykey = config.MY_KEY;
 
-
-
-function getLocation() {
+$(function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getPosition); 
   }
-}
+});
 
 function getPosition(position) {
    var lat = position.coords.latitude;
    var long = position.coords.longitude;
-   var urlApi = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long;
+   // var urlApi = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long;
+   var urlApi = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+ mykey;
+
+   
    // $("#data").html("latitude: " + lat + "<br>longitude: " + long);
   $.getJSON(urlApi, displayWeather);
-
+       
 }
 
 function displayWeather(data) {
   console.log(data);
+  var degSym = '°';
+  var tempScale = 'F';
+  // var currentTemp = 0;
   var weather = data.weather[0].description;
-  var icon = data.weather[0].icon;
-  console.log(weather);
+  // var icon = data.weather[0].icon;
+  var wind = data.wind.speed;
+  var city = data.name;
+  var country = data.sys.country;
+  var currentTemp = data.main.temp;
+  
+  $("#description").html("weather: " + weather + '<br>' +"wind: " + wind + " mph");
+  // $("#image").html("<img src = '" + icon + "'>");  
+  $("#city").text(city + ', ' + country); 
+  $("#temperature").html(kelvinToFahrenheit(currentTemp) + ' °F');
+  $("button").text("to ºC"); 
+  // $("#temp_icon").html("ºC");  
 
-  $("#description").html("weather: " + weather + "<br>icon: " + icon);
+  $("button").click(function() {
+
+   if (tempScale === "F") {
+    $("#temperature").text(kelvinToCelsius(currentTemp) + " " + degSym +  "C");
+    $("button").text("to ºF");
+    tempScale = "C";
+   } else {
+    $("#temperature").text(kelvinToFahrenheit(currentTemp) + " " + degSym  + "F");
+    $("button").text("to ºC");
+    tempScale = "F";
+   } 
+   
+   
 
 
+
+  });
+} 
+
+function kelvinToFahrenheit(t) {
+  return Math.floor((t - 273.15) * 1.8) + 32;
 }
 
-$(getLocation);
+function kelvinToCelsius(t) {
+  return Math.floor(t - 273.15);
+}
+
+
+
 

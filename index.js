ddf033691fc170d70b3eba874662aafd6862263b
Irina <irenekochetkova@gmail.com>
2018-05-03 08:50:17 -0700

    added image

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function getPosition(position) {
    var urlApi = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+ mykey;
 
    
-   // $("#data").html("latitude: " + lat + "<br>longitude: " + long);
-  $.getJSON(urlApi, displayWeather);
+    // $("#data").html("latitude: " + lat + "<br>longitude: " + long);
+   $.getJSON(urlApi, displayWeather);
        
 }
 
@@ -32,23 +32,27 @@ function displayWeather(data) {
   var iconUrl = "http://openweathermap.org/img/w/" + getIcon + ".png";
   $(".icon").html("<img src='" + iconUrl  + "'>");
 
-  // $("#description").html(weather + '<br>' +"wind: " + wind + " mph");
+  $("#description").text(weather);
   $("#city").text(city + ', ' + country); 
   $("#temperature").html(kelvinToFahrenheit(currentTemp) + ' °F');
-  $("button").text("to ºC");
+  $("#buttonId").text("to ºC");
 
-  $("button").click(function() {
+  $("#buttonId").click(function() {
 
    if (tempScale === "F") {
      $("#temperature").text(kelvinToCelsius(currentTemp) + " " + degSym +  "C");
-     $("button").text("to ºF");
+     $("#buttonId").text("to ºF");
     tempScale = "C";
     } else {
      $("#temperature").text(kelvinToFahrenheit(currentTemp) + " " + degSym  + "F");
-     $("button").text("to ºC");
+     $("#buttonId").text("to ºC");
      tempScale = "F";
     } 
   });
+  
+  $("#back").text(imageCode(getIcon));
+  $("#back").css("background-position", "cover");
+
 } 
 
 function kelvinToFahrenheit(t) {
@@ -59,6 +63,41 @@ function kelvinToCelsius(t) {
   return Math.floor(t - 273.15);
 }
 
+function imageCode(conditions) {
+  
+  switch (conditions) {
+    case "01n":
+    case "01d":    
+      document.getElementById("back").style.backgroundImage = "url('clear.jpg')";
+      break;
+    case "02d":
+    case "02n":
+    case "03n":
+    case "03d":
+    case "04d":
+    case "04n":
+      document.getElementById("back").style.backgroundImage = "url('clouds.jpg')";
+      break;
+    case "09d":
+    case "09n":
+    case "10n":
+    case "10d":
+      document.getElementById("back").style.backgroundImage = "url('rain.jpg')";
+      break;
+    case "11d":
+    case "11n":
+      document.getElementById("back").style.backgroundImage = "url('thunderstorm.jpg')";
+      break;
+    case "13d":
+    case "13n":
+      document.getElementById("back").style.backgroundImage = "url('snow.jpg')";
+      break;
+    case "50d":
+    case "50n":
+      document.getElementById("back").style.backgroundImage = "url('mist.jpg')";
+      break;
+   }
+}
 
 
 
